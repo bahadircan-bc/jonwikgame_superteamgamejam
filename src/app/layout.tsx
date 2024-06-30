cb44a9ac11d70e846821abddf9a5edb7477c12ef
Arda Sevinç <arda@ardasevinc.com>
2024-07-01 00:39:29 +0300

    feat: configure fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,16 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import LocalFont from 'next/font/local';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
+const jonWikFont = LocalFont({
+  src: '../assets/fonts/specialagentoutsemital.ttf',
+  declarations: [{ prop: 'format', value: 'truetype' }],
+  style: 'normal',
+  weight: 'normal',
+  variable: '--font-jonwik',
+});
 
 export const metadata: Metadata = {
   title: 'JonWik Game',
@@ -17,7 +25,7 @@ export default function RootLayout({
   return (
     <html
       lang='en'
-      className={`${inter.variable} scroll-smooth antialiased dark`}
+      className={`${inter.variable} ${jonWikFont.variable} scroll-smooth antialiased dark`}
     >
       <body className='text-gray-300 h-dvh'>
         <div className='h-full bg-gradient-to-t from-black to-rose-950'>
