930fdcae3b0e0956ed8081e8fcd6bffbd78da2b9
Arda Sevinç <arda@ardasevinc.com>
2024-07-01 00:50:47 +0300

    feat: add home component

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,1 +1,9 @@
-export default function Home() {}
+import Game from '@/components/game';
+
+export default function Home() {
+  return (
+    <main className='h-full'>
+      <Game />
+    </main>
+  );
+}
